5f1fc323f3b1bf60d00f222c220023c35ba2bcde
Zach Young <ztyoung59@yahoo.com>
2024-07-03 02:13:17 -0500

    placeholder content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+import logo from "../src/assets/site-images/Print_Transparent.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Servanda Advisors</h1>
+      <img src={logo} />
+      <div className="cta">Coming Soon</div>
     </div>
   );
 }
